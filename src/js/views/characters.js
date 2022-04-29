f22b247952b8062197bf33c1087b8e0b49fe0a3e
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-04-29 17:04:16 +0000

    no change|| planets not rendering

--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -12,6 +12,7 @@ export const Characters = () => {
         <div>
             <h1>Character</h1>
             {!store.characters ? <h1>loading </h1> : store.characters.map((character, i) => {
+                console.log(store)
                 return (
                     <div key={character.uid} style={{width: 250}}>
                         <img style={{height: 150, width: 150}} src="https://cdn.vox-cdn.com/thumbor/632XKu70aaOQyRle6NDbklUR9b8=/85x0:1014x619/920x613/filters:focal(85x0:1014x619):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/12771259/ea_star_wars_darth_vader.0.jpg" class="card-img-top" alt="..."/>
